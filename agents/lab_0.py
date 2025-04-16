ac594482181acf09612562a77eb620d1203de830
bar haim <barha@il.ibm.com>
2025-04-16 11:02:09 +0300

    agents beeai lab0

--- agents/lab_0.py
+++ agents/lab_0.py
@@ -7,18 +7,22 @@ from beeai_framework.agents.react import ReActAgent, ReActAgentRunOutput
 from beeai_framework.backend import ChatModel
 from beeai_framework.errors import FrameworkError
 from beeai_framework.memory import UnconstrainedMemory
-from beeai_framework.tools.search import DuckDuckGoSearchTool
 from beeai_framework.tools.weather import OpenMeteoTool
+from beeai_framework.tools.search.wikipedia import WikipediaTool
 
 load_dotenv()
 
 
 async def main() -> None:
+    """
+    Example of using the ReAct agent with a weather tool and watsonx.ai as LLM provider.
+    :return:
+    """
     llm = ChatModel.from_name("watsonx:meta-llama/llama-3-3-70b-instruct")
-    agent = ReActAgent(llm=llm, tools=[DuckDuckGoSearchTool(), OpenMeteoTool()],
+    agent = ReActAgent(llm=llm, tools=[OpenMeteoTool(), WikipediaTool()],
                        memory=UnconstrainedMemory())
 
-    output: ReActAgentRunOutput = await agent.run("What's the current weather in Las Vegas?").on(
+    output: ReActAgentRunOutput = await agent.run("What's the current weather in Tel Aviv? and what the current population in there?").on(
         "update", lambda data, event: print(f"Agent({data.update.key}) 🤖 : ", data.update.parsed_value)
     )
 
