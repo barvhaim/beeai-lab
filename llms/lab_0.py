983dac337a858691fe3fe67f6705f0b7300165d8
bar haim <barha@il.ibm.com>
2025-04-20 18:55:12 +0300

    update lab_0.py to support Ollama LLM and modify parameters for WatsonxLLM

--- llms/lab_0.py
+++ llms/lab_0.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_ibm import WatsonxLLM
+# from langchain_ollama.llms import OllamaLLM
 
 
 load_dotenv()
@@ -8,9 +9,9 @@ load_dotenv()
 
 def lab_0():
     """
-    Sanity check for the running watsonx.ai LLM.
+    Sanity check for the running watsonx.ai LLM or Ollama LLM.
     """
-    parameters = {
+    wx_parameters = {
         "decoding_method": "sample",
         "max_new_tokens": 100,
         "min_new_tokens": 1,
@@ -19,13 +20,26 @@ def lab_0():
         "top_p": 1,
     }
 
-    watsonx_llm = WatsonxLLM(
+    wx_llm = WatsonxLLM(
         model_id="ibm/granite-13b-instruct-v2",
         project_id=os.getenv("WATSONX_PROJECT_ID"),
-        params=parameters,
+        params=wx_parameters,
     )
 
-    response = watsonx_llm.invoke("Who is man's best friend?")
+    # Uncomment the following lines to use Ollama LLM instead
+    # ollama_parameters = {
+    #     "num_predict": 100,  # max_new_tokens
+    #     "temperature": 0.5,
+    #     "top_p": 1,
+    #     "top_k": 50,
+    # }
+    #
+    # ollama_llm = OllamaLLM(
+    #     model="llama3.1:8b",  # ollama pull llama3.1:8b
+    #     **ollama_parameters,
+    # )
+
+    response = wx_llm.invoke("Who is man's best friend?")
     print(response)
 
 
