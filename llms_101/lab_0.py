fc595082cd4f1a7dfe5034f0a26747e54b37ad44
bar haim <barha@il.ibm.com>
2025-05-10 03:25:25 +0300

    Add colored output using termcolor package

--- llms_101/lab_0.py
+++ llms_101/lab_0.py
@@ -1,4 +1,5 @@
 from llm_provider import get_llm_client
+from termcolor import colored
 
 
 def lab_0():
@@ -9,25 +10,26 @@ def lab_0():
         "decoding_method": "sample",
         "max_tokens": 100,
         "min_tokens": 1,
-        "temperature": 0.5,
+        "temperature": 0.05,
         "top_k": 50,
         "top_p": 1,
     }
 
-    # (watsonx.ai)
+    # model_name = "ibm/granite-13b-instruct-v2"  # watsonx.ai
+    model_name = "llama3.1:8b"  # ollama
+
     llm = get_llm_client(
-        model_name="ibm/granite-13b-instruct-v2",
+        model_name=model_name,
         model_parameters=parameters
     )
 
-    # (ollama)
-    # llm = get_llm_client(
-    #     model_name="llama3.1:8b",
-    #     model_parameters=parameters
-    # )
-
     response = llm.invoke("Who is man's best friend?")
-    print(response)
+    print(colored(response, 'green'))
+
+    # Additional tasks:
+    # 1. Change the llm provider to use "llama3.1:8b" from ollama, make sure you've pulled the model before running! (`ollama pull llama3.1:8b`) and set the `.env` file to use ollama.
+    # 2. Change the model parameters to make the model more creative
+    # 3. Use a different model from watsonx.ai/ollama
 
 
 if __name__ == "__main__":
