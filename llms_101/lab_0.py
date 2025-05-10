1d26e0c61650baa9665642d67682395e77220ce3
bar haim <barha@il.ibm.com>
2025-05-10 03:09:37 +0300

    ollama example
    Signed-off-by: bar haim <barha@il.ibm.com>

--- llms_101/lab_0.py
+++ llms_101/lab_0.py
@@ -1,45 +1,32 @@
-import os
-from dotenv import load_dotenv
-from langchain_ibm import WatsonxLLM
-# from langchain_ollama.llms import OllamaLLM
-
-
-load_dotenv()
+from llm_provider import get_llm_client
 
 
 def lab_0():
     """
     Sanity check for the running watsonx.ai LLM or Ollama LLM.
     """
-    wx_parameters = {
+    parameters = {
         "decoding_method": "sample",
-        "max_new_tokens": 100,
-        "min_new_tokens": 1,
+        "max_tokens": 100,
+        "min_tokens": 1,
         "temperature": 0.5,
         "top_k": 50,
         "top_p": 1,
     }
 
-    wx_llm = WatsonxLLM(
-        model_id="ibm/granite-13b-instruct-v2",
-        project_id=os.getenv("WATSONX_PROJECT_ID"),
-        params=wx_parameters,
+    # (watsonx.ai)
+    llm = get_llm_client(
+        model_name="ibm/granite-13b-instruct-v2",
+        model_parameters=parameters
     )
 
-    # Uncomment the following lines to use Ollama LLM instead
-    # ollama_parameters = {
-    #     "num_predict": 100,  # max_new_tokens
-    #     "temperature": 0.5,
-    #     "top_p": 1,
-    #     "top_k": 50,
-    # }
-    #
-    # ollama_llm = OllamaLLM(
-    #     model="llama3.1:8b",  # ollama pull llama3.1:8b
-    #     **ollama_parameters,
+    # (ollama)
+    # llm = get_llm_client(
+    #     model_name="llama3.1:8b",
+    #     model_parameters=parameters
     # )
 
-    response = wx_llm.invoke("Who is man's best friend?")
+    response = llm.invoke("Who is man's best friend?")
     print(response)
 
 
---
+++ llms_101/llm_provider/__init__.py
@@ -0,0 +1,73 @@
+import os
+from typing import Dict, Optional, Any
+from dotenv import load_dotenv
+from llm_provider.provider_type import LLMProviderType
+
+load_dotenv()
+
+LLM_PROVIDER = LLMProviderType(os.getenv("LLM_PROVIDER", LLMProviderType.WATSONX.value))
+
+
+def _get_base_llm_settings(model_name: str, model_parameters: Optional[Dict]) -> Dict:
+    if model_parameters is None:
+        model_parameters = {}
+
+    if LLM_PROVIDER == LLMProviderType.OLLAMA:
+        parameters = {
+            "num_predict": model_parameters.get("max_tokens", 100),
+            "temperature": model_parameters.get("temperature", 0.9),
+            "top_p": model_parameters.get("top_p", 1.0),
+            "top_k": model_parameters.get("top_k", 50),
+        }
+
+        return {
+            "model": model_name,
+            **parameters,
+        }
+
+    elif LLM_PROVIDER == LLMProviderType.WATSONX:
+        parameters = {
+            "min_new_tokens": model_parameters.get("min_tokens", 1),
+            "max_new_tokens": model_parameters.get("max_tokens", 100),
+            "decoding_method": model_parameters.get("decoding_method", "greedy"),
+            "temperature": model_parameters.get("temperature", 0.9),
+            "repetition_penalty": model_parameters.get("repetition_penalty", 1.0),
+            "top_k": model_parameters.get("top_k", 50),
+            "top_p": model_parameters.get("top_p", 1.0),
+            "stop_sequences": model_parameters.get("stop_sequences", []),
+        }
+
+        return {
+            "url": os.getenv("WATSONX_URL"),
+            "project_id": os.getenv("WATSONX_PROJECT_ID"),
+            "apikey": os.getenv("WATSONX_APIKEY"),
+            "model_id": model_name,
+            "params": parameters,
+        }
+
+    raise ValueError(f"Incorrect LLM provider: {LLM_PROVIDER}")
+
+
+def get_llm_client(
+        model_name: str = "meta-llama/llama-3-3-70b-instruct",
+        model_parameters: Optional[Dict] = None,
+) -> Any:
+    if LLM_PROVIDER == LLMProviderType.WATSONX:
+        from langchain_ibm import WatsonxLLM
+
+        return WatsonxLLM(
+            **_get_base_llm_settings(
+                model_name=model_name,
+                model_parameters=model_parameters
+            )
+        )
+
+    elif LLM_PROVIDER == LLMProviderType.OLLAMA:
+        from langchain_ollama.llms import OllamaLLM
+
+        return OllamaLLM(
+            **_get_base_llm_settings(
+                model_name=model_name, model_parameters=model_parameters
+            )
+        )
+    return None
