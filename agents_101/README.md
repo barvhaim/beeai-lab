d6ffcf1529f7d5ff95a837861ec0d8bd427e354d
bar haim <barha@il.ibm.com>
2025-05-11 12:00:04 +0300

    agents lab

--- agents_101/README.md
+++ agents_101/README.md
@@ -3,27 +3,26 @@ _Powering the future of open-source AI agent development_
 
 Based on https://github.com/i-am-bee
 
-* **BeeAI platform** - The platform to easily discover, run, and compose AI agents from any framework. 
-* **BeeAI framework** - A production-grade framework for building AI agents in either Python or TypeScript. 
+* **BeeAI Platform** - The platform to easily discover, run, and compose AI agents from any framework. 
+* **BeeAI Framework** - A production-grade framework for building AI agents in either Python or TypeScript. 
 * **Agent Communication Protocol (ACP)** - The standard for agent-to-agent communication, built for automation, collaboration, and UI integration.
 * **Model Context Protocol (MCP)** - Give AI agents a consistent way to connect with tools, services, and data — no matter where they live or how they're built.
 
 ## ⚙️ Setup
-Copy the `.env.example` file to `.env` and fill in the relevant values:
+1. Copy the `.env.example` file to `.env` and fill in the relevant values:
 ```bash
 cp .env.example .env
 ```
-
-## 🧪 Labs
-- [Lab 0](./lab_0.py) - Demonstrates the use of the ReAct agent with weather and Wikipedia tools.
-- [Lab 1.1](./lab_1_1.py) - Integrates the ReAct agent with VirusTotal tools using MCP for domain and IP analysis.
-- [Lab 1.2](./lab_1_2.py) - Uses the ReAct agent with Tavily tools for enhanced functionality via MCP.
-- [Lab 2](./lab_2.py) - Showcases the ReAct agent with a custom Threat Intelligence tool.
-- [Lab 3](./lab_3.md) - Work in progress. BeeAI is rebuilding their agent SDK.
-- [Lab 4](./lab_4.py) - Implements a multi-agent workflow for research, weather forecasting, and data synthesis.
+- `WATSONX_URL` - if using `watsonx.ai` as LLM provider
+- `WATSONX_PROJECT_ID` - if using `watsonx.ai` as LLM provider
+- `WATSONX_API_KEY` - if using `watsonx.ai` as LLM provider
+- `VIRUSTOTAL_API_KEY` - For VirusTotal tools
+- `TAVILY_API_KEY` - For Tavily (Search) tools
 
 
-## BeeAI platform
+2. Some labs uses Local MCP servers. These servers requires `Node.js` to run. Recommended is working with `nvm` to switch between Node.js versions - [https://nodejs.org/en/download](https://nodejs.org/en/download) (Tested with version 20)
+
+### Install BeeAI Platform (optional)
 0. Start [Ollama](https://ollama.com/)
 1. **Install** BeeAI using [Homebrew](https://brew.sh/) (or see the [installation guide](https://docs.beeai.dev/introduction/installation) for other methods):
 
@@ -31,43 +30,43 @@ cp .env.example .env
 brew install i-am-bee/beeai/beeai
 brew services start beeai
 ```
+for stopping the service use:
+```sh
+brew services stop beeai
+```
 
-2. **Configure** LLM provider:
-
+2. **Configure** LLM provider (once):
 ```sh
 beeai env setup
 ```
 
-3. **Launch** the web interface:
+#### Ollama config:
+- Choose LLM provider: Ollama 💻 local.
+- Use the recommended model `llama3.1:8b`.
+- Context window size of at least 8k tokens.
 
-```sh
-beeai ui
-```
-
-4. **Use** from the terminal:
-
-```sh
-# List commands
-beeai --help
-
-# List all available agents
-beeai list
-
-# Run the chat agent
-beeai run chat
-```
-
-### Litellm + BeeAI config
-- URL: http://localhost:4000
+#### watsonx.ai config:
+- Clone the repo and follow the [instructions](https://github.com/barvhaim/beeai-litellm-watsonx).
+- Choose LLM provider: Other 🔧 provide API URL.
+- base URL: http://localhost:4000
 - API Key: `dummy`
 - Model: `meta-llama/llama-3-3-70b-instruct`
+
+
+## 🧪 Labs
+- [Lab 0](./lab_0.py) - Demonstrates the use of the ReAct agent with weather and search tools.
+- [Lab 1.1](./lab_1_1.py) - Integrates the ReAct agent with VirusTotal tools using MCP for domain and IP analysis.
+- [Lab 1.2](./lab_1_2.py) - Uses the ReAct agent with Tavily tools for enhanced functionality via MCP.
+- [Lab 2](./lab_2.py) - Showcases the ReAct agent with a custom Threat Intelligence tool.
+- [Lab 3](./lab_3.md) - Work in progress. BeeAI is rebuilding their agent SDK.
+- [Lab 4](./lab_4.py) - Implements a multi-agent workflow for research, weather forecasting, and data synthesis.
 
 
 # 🤖 AI Agents with watsonx Orchestrate
 TBD
 
 
-## More readings
+## 📖 Further readings
 - [BeeAI docs](https://docs.beeai.dev/)
 - [IBM AgentLab](https://www.ibm.com/docs/en/watsonx/saas?topic=solutions-agent-lab-beta)
 - [watsonx Orchestrate](https://www.ibm.com/products/watsonx-orchestrate)
--- agents_101/lab_0.py
+++ agents_101/lab_0.py
@@ -1,57 +1,99 @@
+from typing import Any
 from dotenv import load_dotenv
 import asyncio
 import sys
 import traceback
-import argparse
 
-from beeai_framework.agents.react import ReActAgent, ReActAgentRunOutput
+from beeai_framework.agents.react import ReActAgent
 from beeai_framework.backend import ChatModel, ChatModelParameters
 from beeai_framework.errors import FrameworkError
-from beeai_framework.memory import UnconstrainedMemory
+from beeai_framework.memory import TokenMemory
+from beeai_framework.tools import AnyTool
 from beeai_framework.tools.weather import OpenMeteoTool
 from beeai_framework.tools.search.wikipedia import WikipediaTool
+from beeai_framework.tools.search.duckduckgo import DuckDuckGoSearchTool
+from beeai_framework.agents import AgentExecutionConfig
+from beeai_framework.emitter import EmitterOptions, EventMeta
+from utils.io import ConsoleReader
 
 load_dotenv()
 
+reader = ConsoleReader()
 
-async def main(prompt: str) -> None:
-    """
-    Example of using the ReAct agent with a weather tool and a Wikipedia search tool.
-    :param prompt: The prompt to provide to the agent.
-    """
+
+def _create_agent() -> ReActAgent:
+    """Create and configure the agent with tools and LLM"""
+
     # Other models to try:
     # "ollama:llama3.1"
     # "ollama:granite3.1-dense:8b"
     # "watsonx:meta-llama/llama-3-3-70b-instruct"
     # with Ollama, ensure the model is pulled before running.
     # with watsonx.ai, ensure relevant ENV is set.
+    model_name = "ollama:granite3.1-dense:8b"
+    # model_name = "watsonx:meta-llama/llama-3-3-70b-instruct"
+
     llm = ChatModel.from_name(
-        "ollama:llama3.1",
+        model_name,
         ChatModelParameters(temperature=0),
     )
-    agent = ReActAgent(
-        llm=llm, tools=[OpenMeteoTool(), WikipediaTool()], memory=UnconstrainedMemory()
-    )
 
-    output: ReActAgentRunOutput = await agent.run(prompt).on(
-        "update",
-        lambda data, event: print(
-            f"Agent({data.update.key}) 🤖 : ", data.update.parsed_value
-        ),
-    )
+    # Configure tools
+    tools: list[AnyTool] = [
+        WikipediaTool(),
+        OpenMeteoTool(),
+        DuckDuckGoSearchTool()
+    ]
 
-    print("Agent 🤖 : ", output.result.text)
+    # Create a ReAct agent with memory (https://github.com/i-am-bee/beeai-framework/blob/main/python/docs/memory.md#overview) 
+    # and tools (https://github.com/i-am-bee/beeai-framework/blob/main/python/docs/tools.md#built-in-tools)
+    agent = ReActAgent(llm=llm, tools=tools, memory=TokenMemory(llm))
+    return agent
+
+
+def _process_agent_events(data: Any, event: EventMeta) -> None:
+    """Process agent events and log appropriately"""
+
+    if event.name == "error":
+        reader.write("Agent 🤖 : ", FrameworkError.ensure(data.error).explain())
+    elif event.name == "retry":
+        reader.write("Agent 🤖 : ", "retrying the action...")
+    elif event.name == "update":
+        reader.write(f"Agent({data.update.key}) 🤖 : ", data.update.parsed_value)
+    elif event.name == "start":
+        reader.write("Agent 🤖 : ", "starting new iteration")
+    elif event.name == "success":
+        reader.write("Agent 🤖 : ", "success")
+
+
+async def lab_0() -> None:
+    """
+    Example of using the ReAct agent with a weather tool, Wikipedia tool and DuckDuckGo search tool.
+    """
+    
+    agent = _create_agent()
+
+    reader.write("🛠️ System: ", "Agent initialized with Wikipedia, DuckDuckGo, and Weather tools.")
+
+    # Main interaction loop with user input
+    for prompt in reader:
+        # Run agent with the prompt
+        response = await agent.run(
+            prompt=prompt,
+            execution=AgentExecutionConfig(max_retries_per_step=3, total_max_retries=10, max_iterations=20),
+        ).on("*", _process_agent_events, EmitterOptions(match_nested=False))
+
+        reader.write("Agent 🤖 : ", response.result.text)
+        
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Run the ReAct agent with a custom prompt."
-    )
-    parser.add_argument("prompt", type=str, help="The prompt to provide to the agent.")
-    args = parser.parse_args()
-
     try:
-        asyncio.run(main(args.prompt))
+        asyncio.run(lab_0())
     except FrameworkError as e:
         traceback.print_exc()
         sys.exit(e.explain())
+
+
+# Tasks:
+# 1. Try to run the lab with either `ollama` or `watsonx.ai` as LLM provider. Inspect the thought process of the agent.
